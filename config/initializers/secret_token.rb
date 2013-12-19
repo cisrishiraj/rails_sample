e741704711a243e5d647b00c1ca4fb34bb177e2f
Rishiraj <rishiraj.v@cisinlabs.com>
2013-12-19 17:34:11 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rishitest::Application.config.secret_key_base = 'e2607c01a9a44922922d0b0f2062d3e39dd09f78379c004448a349aceaad67cffcf2ab4f5871f77057a850ffdeca599cea1ec07012a62cbbaab31d06b96fd7d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rishitest::Application.config.session_store :cookie_store, key: '_rishitest_session'
